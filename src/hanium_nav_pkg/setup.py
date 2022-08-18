0dac18667da09c76020fe5b9aaa7a527ecac50f1
HJ <ohj_980918@naver.com>
2022-08-18 19:05:23 +0900

    자율주행 패키지 소스코드 업데이트

--- src/hanium_nav_pkg/setup.py
+++ src/hanium_nav_pkg/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'navigation_hanium=hanium_nav_pkg.navigation_hanium:main'
         ],
     },
 )
