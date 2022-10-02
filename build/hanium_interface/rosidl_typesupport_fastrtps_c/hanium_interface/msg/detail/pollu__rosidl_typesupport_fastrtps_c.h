3a2c7220ecf05c76ec4d1a5093a9d909ac7c0b0b
HJ <ohj_980918@naver.com>
2022-10-03 04:39:12 +0900

    stella package delete

---
+++ build/hanium_interface/rosidl_typesupport_fastrtps_c/hanium_interface/msg/detail/pollu__rosidl_typesupport_fastrtps_c.h
@@ -0,0 +1,36 @@
+// generated from rosidl_typesupport_fastrtps_c/resource/idl__rosidl_typesupport_fastrtps_c.h.em
+// with input from hanium_interface:msg/Pollu.idl
+// generated code does not contain a copyright notice
+#ifndef HANIUM_INTERFACE__MSG__DETAIL__POLLU__ROSIDL_TYPESUPPORT_FASTRTPS_C_H_
+#define HANIUM_INTERFACE__MSG__DETAIL__POLLU__ROSIDL_TYPESUPPORT_FASTRTPS_C_H_
+
+
+#include <stddef.h>
+#include "rosidl_runtime_c/message_type_support_struct.h"
+#include "rosidl_typesupport_interface/macros.h"
+#include "hanium_interface/msg/rosidl_typesupport_fastrtps_c__visibility_control.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_hanium_interface
+size_t get_serialized_size_hanium_interface__msg__Pollu(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_hanium_interface
+size_t max_serialized_size_hanium_interface__msg__Pollu(
+  bool & full_bounded,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_FASTRTPS_C_PUBLIC_hanium_interface
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_fastrtps_c, hanium_interface, msg, Pollu)();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // HANIUM_INTERFACE__MSG__DETAIL__POLLU__ROSIDL_TYPESUPPORT_FASTRTPS_C_H_
